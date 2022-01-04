6600879703aaa24ed1f99ca53c1ae1b29889fd85
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-01-04 10:21:07 +0100

    setup Rest-API-Service

--- weather_tracker/README.md
+++ weather_tracker/README.md
@@ -17,3 +17,8 @@ Ready to run in production? Please [check our deployment guides](https://hexdocs
   * Docs: https://hexdocs.pm/phoenix
   * Forum: https://elixirforum.com/c/phoenix-forum
   * Source: https://github.com/phoenixframework/phoenix
+
+## Before starting the Phoenix server, you have to start the Timescale-DB Docker Container
+
+* docker-compose up
+
--- weather_tracker/lib/weather_tracker_web/router.ex
+++ weather_tracker/lib/weather_tracker_web/router.ex
@@ -2,11 +2,13 @@ defmodule WeatherTrackerWeb.Router do
   use WeatherTrackerWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", WeatherTrackerWeb do
-    pipe_through :api
+    pipe_through(:api)
+
+    post("/weather-conditions", WeatherConditionsController, :create)
   end
 
   # Enables the Swoosh mailbox preview in development.
@@ -15,9 +17,9 @@ defmodule WeatherTrackerWeb.Router do
   # node running the Phoenix server.
   if Mix.env() == :dev do
     scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
+      pipe_through([:fetch_session, :protect_from_forgery])
 
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
