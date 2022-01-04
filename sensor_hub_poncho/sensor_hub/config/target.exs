92c55b11955e8e85b8638b4cf2867c20c571d23e
Heiko Goes <heiko.goes@gaebler-goes.de>
2022-01-04 12:17:37 +0100

    fixed sensor_hub weather_tracker_url

--- sensor_hub_poncho/sensor_hub/config/target.exs
+++ sensor_hub_poncho/sensor_hub/config/target.exs
@@ -90,7 +90,7 @@ config :mdns_lite,
     }
   ]
 
-config :sensor_hub, :weather_tracker_url, "http://192.168.178.70:4000/api/weather-conditions"
+config :sensor_hub, :weather_tracker_url, "http://192.168.178.90:4000/api/weather-conditions"
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
